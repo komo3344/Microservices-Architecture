a9bf3032d6c6ebabde298c7450b19a03950dc30a
komo3344 <jy940608@naver.com>
2019-12-23 14:41:51 +0900

    API 인증추가

--- MSA_Project/app/views.py
+++ MSA_Project/app/views.py
@@ -1,8 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import User
 from .serializers import UserSerializer
 
 
 class UserList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
