07a85a4ab2ca4770f17e21d531e90d88f2519be6
komo3344 <jy940608@naver.com>
2019-12-27 17:59:53 +0900

    create vue project & add login page

---
+++ Vue_Project/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <div>
+
+      <h1 class="title">
+        Hi~! vue_project
+      </h1>
+      <h2 class="subtitle">
+        Hello~ microservices architecture toy project 
+      </h2>
+
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+    
+  }
+}
+</script>
+
---
+++ Vue_Project/pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <h3>Login</h3>
+    <input type="text" placeholder="ID를 입력하세요"><br>
+    <input type="password" placeholder="비밀번호를 입력하세요"><br>
+    <button>로그인</button>
+    <p>계정이 없으신가요? 회원가입하러 가기</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data(){
+    return{
+
+    }
+  },
+  methods: {}
+}
+</script>
